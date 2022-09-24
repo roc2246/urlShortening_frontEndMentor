435843964b8b68a0d1050608c75584047669aa23
Riley <riley.childs@yahoo.com>
2022-09-24 09:51:54 -0400

    Simplified code for copyURL()

--- src/components/URL.svelte
+++ src/components/URL.svelte
@@ -5,17 +5,12 @@ export let before
 export let after
 
 const copyURL = (url) =>{
-    let copiedURL = Object.values(url)
-    copiedURL = JSON.stringify(copiedURL)
-    copiedURL = copiedURL.replace('["', "");
-    copiedURL = copiedURL.replace('"]', "");
-
-    navigator.clipboard.writeText(copiedURL);
+    navigator.clipboard.writeText(url);
 }
 </script>
 
 <div class="shortened-url">
     <span class="shortened-url--before">{before}</span>
     <span class="shortened-url--after">{after}</span>
-    <Button className="btn btn--copy" on:click={()=>copyURL({after})}>Copy</Button>
+    <Button className="btn btn--copy" on:click={()=>copyURL(after)}>Copy</Button>
 </div>
