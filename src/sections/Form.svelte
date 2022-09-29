aa446d6e4484f0701a99887f51716e71475c352c
Riley <riley.childs@yahoo.com>
2022-09-29 15:20:45 -0400

    Fixed form UI

--- src/sections/Form.svelte
+++ src/sections/Form.svelte
@@ -2,7 +2,7 @@
   import Button from "../components/Button.svelte";
   import URL from "../components/URL.svelte";
 
-  let displayError = false
+  let displayError = false;
 
   let shortURLs = [];
 
@@ -32,10 +32,10 @@
       /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     if (input.match(URLRegex)) {
       shortener(input);
-      displayError = false
+      displayError = false;
     } else {
       inputBox.style.borderColor = "hsl(0, 87%, 67%)";
-      displayError = true
+      displayError = true;
     }
     inputBox.value = "";
   };
@@ -43,64 +43,72 @@
 
 <section class="shortener-container">
   <form class="shortener" on:submit|preventDefault>
-  <div class="insert-url">
-    <input type="text" placeholder="Shorten a link here..." class="url--before" />
-    {#if displayError === true}
-    <p class="shortener__error-message">Please <i>add</i> a link</p>
-    {/if}
-  </div>
-  
-  <Button className="btn btn--shorten" on:click={() => isURL()}
-    >Shorten It!</Button
-  >
+    <div class="insert-url">
+      <input
+        type="text"
+        placeholder="Shorten a link here..."
+        class="url--before"
+      />
+      {#if displayError === true}
+        <p class="shortener__error-message">Please <i>add</i> a link</p>
+      {/if}
+    </div>
 
-</form>
-
+    <div class="button-container">
+      <Button className="btn btn--shorten" on:click={() => isURL()}
+        >Shorten It!</Button
+      >
+    </div>
+  </form>
 </section>
 
 <section class="url-container">
- {#each shortURLs as url, i}
-  <URL before={url.before} after={url.after} btnID={i} />
-{/each} 
+  {#each shortURLs as url, i}
+    <URL before={url.before} after={url.after} btnID={i} />
+  {/each}
 </section>
-
 
 <style lang="scss">
   @import "../scss/global";
-  .shortener-container{
-    background: linear-gradient(to bottom,  white 50%,$gray 50%,);
+  .shortener-container {
+    background: linear-gradient(to bottom, white 50%, $gray 50%);
   }
   .shortener {
     margin-right: min(10rem, 5%);
     margin-left: min(10rem, 5%);
     background-image: url("/images/bg-boost-desktop.svg");
     background-color: $very-dark-blue;
-    border-radius: .25rem;
+    border-radius: 0.25rem;
     padding: 2rem;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: space-between;
-    .insert-url{
+    height: 2.5rem;
+    .insert-url {
       display: flex;
       flex-direction: column;
       width: 80%;
     }
-    &__error-message{
+    &__error-message {
       margin-top: 0;
       color: $red;
     }
   }
-  .url-container{
+  .url-container {
     background-color: $gray;
     padding-top: 1rem;
     padding-bottom: 1rem;
   }
-  @media (max-width: 375px){
-    .shortener{
-    .insert-url {
-    width: 100%;
-  }
-}
+  @media (max-width: 375px) {
+    .shortener {
+      height: auto;
+      .insert-url {
+        width: 100%;
+      }
+      .button-container {
+        width: 100%;
+      }
+    }
   }
 </style>
